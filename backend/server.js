5cd2f96e0c1a514fb84cfdf2bf3e5386df2c1d71
Punitjadhav07 <jadhavpunit30@gmail.com>
2025-12-28 18:00:56 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
+
+app.get('/todolist', (req, res) => {
+    res.send('Hello World!')
+})
+
 const port = 3000   
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
