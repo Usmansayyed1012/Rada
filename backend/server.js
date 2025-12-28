cc02be0736f8957221e06b366106b4cc99dc2d63
Punitjadhav07 <jadhavpunit30@gmail.com>
2025-12-28 18:07:23 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
+const db = mysql.createConnection({
+  host: 'ciacloud,in',
+  user: 'tpuser',
+  password: '%TGBbgt5',
+  database: 'tpdb'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('❌ Database connection failed:', err.message);
+    return;
+  }
+  console.log('✅ MySQL Connected...');
+});
+
+module.exports = db;
 
 app.get('/todolist', (req, res) => {
     res.send('Hello World!')
